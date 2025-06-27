4c016d1fa96db0acf44394d30edffcf3be92a0e2
Noel Sebu <noelsebu555@gmail.com>
2025-06-27 22:40:59 +0530

    import and use ProjectSection component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '@/components/Navbar'
 import ProfileAndBio from '@/components/ProfileAndBio'
+import ProjectsSection from '@/components/ProjectsSection'
 
 const page = () => {
   return (
-    <div>
+    <div className='overflow-x-hidden'>
       <Navbar />
       <ProfileAndBio />
+      <ProjectsSection />
     </div>
   )
 }
