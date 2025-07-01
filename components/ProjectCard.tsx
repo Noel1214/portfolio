05ef18cc1ee1265a05a1c1a22716b14bab65ab62
Noel Sebu <noelsebu555@gmail.com>
2025-07-01 10:15:35 +0530

    Completed project card

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -4,32 +4,31 @@ import React, { useState } from "react";
 import "../components/css/ProjectCard.css";
 
 const ProjectCard = () => {
-  const [first, setfirst] = useState(false);
 
   return (
-    <div
-      className={`interpolate-property flex justify-center bg-green-500 rounded-2xl transition-all duration-300 ease-in-out sm:max-h-none sm:max-w-none overflow-hidden ${
-        first
-          ? "sm:pt-5 h-auto w-[80vw] sm:w-[85vw] max-w-[210rem]"
-          : "h-[10rem] w-[60vw] sm:h-[30vw] sm:w-[45vw] hover:scale-110 max-h-[90vh] max-w-[21rem]"
-      }`}
-      onClick={() => {
-        setfirst((prev) => !prev);
-      }}
-    >
-      <div className="flex flex-col gap-16 h-full w-full items-center rounded-2xl">
+    <div className="interpolate-property bg-neutral-900 w-[20rem] h-[25rem] sm:w-[24rem] flex justify-center rounded-2xl transition-all duration-300 ease-in-out sm:max-h-none sm:max-w-none overflow-hidden hover:scale-110">
+      <div className="flex flex-col gap-2 h-full w-full items-center rounded-2 xl">
         <Image
-          className="h-[12rem] sm:h-[30vw] sm:w-[45vw] rounded-2xl shrink-0"
+          className="h-[11rem] w-[20rem] sm:h-[13rem] sm:w-[24rem] rounded-2xl"
           src="/tut-app.png"
           width={500}
           height={500}
           alt="Picture of the project"
         />
-        <div className="bg-red-500 h-auto min-h-[10vw] w-[50vw] ov erflow-auto">
-          <h1>
-            Desc: A web app to display and get reviews of tution tution teachers
-            of Excellent tution center
+        <div className="h-auto p-2 flex flex-col gap-6 scale-95">
+          <h1 className="text-xl underline underline-offset-7">Tution App</h1>
+          <h1 className="">
+            A web app to display and get reviews of tution tution
+            teachers of a tution center
           </h1>
+          <div className="flex gap-3">
+            <button className="h-10 w-28 bg-neutral-700 rounded-3xl text-white  hover:cursor-pointer">
+              View Demo
+            </button>
+            <button className="h-10 w-28 bg-neutral-700 rounded-3xl text-white hover:cursor-pointer">
+              View Code
+            </button>
+          </div>
         </div>
       </div>
     </div>
