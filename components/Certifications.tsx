09320f6146d40f1ac8a20c4a3fef6c8852cf0834
Noel Sebu <noelsebu555@gmail.com>
2025-07-01 11:30:45 +0530

    certifiaction section created

---
+++ components/Certifications.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Certifications = () => {
+  return (
+    <div className="w-screen min-h-[50vh] flex flex-col gap-8 items-center font-sans">
+      <h1
+        className="text-5xl text-center font-bold bg-gradient-to-r from-cyan-400 to-blue-500 bg-clip-text text-transparent"
+      >
+        Certifications & Achievements.
+      </h1>
+      <h1 className="w-[75vw] text-center text-lg">
+        Continuous learning is the key to staying current with evolving
+        technology. Here are my professional certifications and achievements
+        that demonstrate my expertise.
+      </h1>
+    </div>
+  );
+};
+
+export default Certifications;
