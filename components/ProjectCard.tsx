5bb7cf9960b8a310f1bdb2633f3675c4a5066473
Noel Sebu <noelsebu555@gmail.com>
2025-07-01 12:42:59 +0530

    gap modified

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -9,13 +9,13 @@ const ProjectCard = () => {
     <div className="interpolate-property bg-neutral-900 w-[20rem] h-[25rem] sm:w-[24rem] flex justify-center rounded-2xl transition-all duration-300 ease-in-out sm:max-h-none sm:max-w-none overflow-hidden hover:scale-105">
       <div className="flex flex-col gap-2 h-full w-full items-center rounded-2 xl">
         <Image
-          className="h-[11rem] w-[20rem] sm:h-[13rem] sm:w-[24rem] rounded-2xl"
+          className="h-[11rem] w-[20rem] sm:h-[13rem] sm:w-[24rem]"
           src="/tut-app.png"
           width={500}
           height={500}
           alt="Picture of the project"
         />
-        <div className="h-auto p-2 flex flex-col gap-6 scale-95">
+        <div className="h-auto p-2 flex flex-col gap-9 sm:gap-6 scale-95">
           <h1 className="text-xl underline underline-offset-7">Tution App</h1>
           <h1 className="">
             A web app to display and get reviews of tution tution
