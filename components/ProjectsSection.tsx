c8b16ae4841acf0e897f64daa65dbc9e95284c7b
Noel Sebu <noelsebu555@gmail.com>
2025-07-01 10:13:39 +0530

    removed margin

--- components/ProjectsSection.tsx
+++ components/ProjectsSection.tsx
@@ -3,15 +3,11 @@ import ProjectCard from "./ProjectCard";
 
 const ProjectsSection = () => {
   return (
-      <div className="mt-3 h-screen">
+      <div className="h-screen">
       <div className="flex justify-center">
         <h1 className="text-2xl sm:text-3xl text-center font-semibold">Featured Projects</h1>
       </div>
-      <div className="px-5 pb-[7rem] mt-16 h-auto flex flex-col gap-24 items-center justify-center">
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
+      <div className="px-6 pb-[7rem] mt-16 h-auto flex flex-wrap gap-24 items-start justify-center">
         <ProjectCard />
       </div>
     </div>
