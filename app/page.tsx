2419e5f3c1302f11e825b0432b4df1416e5a75f0
Noel Sebu <noelsebu555@gmail.com>
2025-07-01 11:30:20 +0530

    certification section added to page

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,17 @@
-import React from 'react'
-import Navbar from '@/components/Navbar'
-import ProfileAndBio from '@/components/ProfileAndBio'
-import ProjectsSection from '@/components/ProjectsSection'
+import React from "react";
+import Navbar from "@/components/Navbar";
+import ProfileAndBio from "@/components/ProfileAndBio";
+import ProjectsSection from "@/components/ProjectsSection";
+import Certifications from "@/components/Certifications";
 
 const page = () => {
   return (
-    <div className='overflow-x-hidden'>
+    <div className="overflow-x-hidden">
       <ProfileAndBio />
       <ProjectsSection />
+      <Certifications />
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
