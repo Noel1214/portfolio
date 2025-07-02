7f9f9a583ba7d60e833150d563de0f8ace1fab63
Noel Sebu <noelsebu555@gmail.com>
2025-07-02 10:53:38 +0530

    added date to project card

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -2,32 +2,48 @@
 import Image from "next/image";
 import React, { useState } from "react";
 import "../components/css/ProjectCard.css";
+import { CiCalendar } from "react-icons/ci";
 
-const ProjectCard = () => {
+interface CertificateCardProps {
+  imageLink: string;
+  title: string;
+  demo: string;
+  desc: string;
+  code: string;
+  date: string;
+}
 
+const ProjectCard: React.FC<CertificateCardProps> = (props) => {
   return (
-    <div className="interpolate-property bg-neutral-900 w-[20rem] h-[25rem] sm:w-[24rem] flex justify-center rounded-2xl transition-all duration-300 ease-in-out sm:max-h-none sm:max-w-none overflow-hidden hover:scale-105">
-      <div className="flex flex-col gap-2 h-full w-full items-center rounded-2 xl">
+    <div className="px-4 pt-3 pb-5 bg-neutral-900 w-[20rem] min-h-[25rem] sm:w-[24rem] flex justify-center rounded-2xl hover:scale-105 transition-all duration-300 ease-in-out overflow-hidden">
+      <div className="flex flex-col gap- h-full w-full items-center">
         <Image
-          className="h-[11rem] w-[20rem] sm:h-[13rem] sm:w-[24rem]"
-          src="/tut-app.png"
+          className="h-[11rem] w-[20rem] sm:h-[13rem] sm:w-[24rem] object-contain"
+          src={`/${props.imageLink}`}
           width={500}
           height={500}
           alt="Picture of the project"
         />
-        <div className="h-auto p-2 flex flex-col gap-9 sm:gap-6 scale-95">
-          <h1 className="text-xl underline underline-offset-7">Tution App</h1>
-          <h1 className="">
-            A web app to display and get reviews of tution tution
-            teachers of a tution center
-          </h1>
-          <div className="flex gap-3">
-            <button className="h-10 w-28 bg-neutral-700 rounded-3xl text-white  hover:cursor-pointer">
-              View Demo
-            </button>
-            <button className="h-10 w-28 bg-neutral-700 rounded-3xl text-white hover:cursor-pointer">
-              View Code
-            </button>
+        <div className="h-auto flex flex-col gap-4 scale-95">
+          <div className="flex flex-col gap-4">
+            <h1 className="text-2xl">{props.title}</h1>
+            <div className="text-sm flex items-center gap-1">
+              <CiCalendar size={20} />
+              <h1 className="translate-y-[0.1rem]">completed on {props.date}</h1>
+            </div>
+          </div>
+          <h1 className="">{props.desc}</h1>
+          <div className="flex gap-4">
+            <a target="_blank" href={props.demo}>
+              <button className="h-10 px-3 bg-neutral-700 rounded-3xl text-white  hover:cursor-pointer hover:scale-105 transition-all duration-200 ease-in-out">
+                View Demo
+              </button>
+            </a>
+            <a target="_blank" href={props.code}>
+              <button className="h-10 px-3 bg-neutral-700 rounded-3xl text-white  hover:cursor-pointer hover:scale-105 transition-all duration-200 ease-in-out">
+                View Code
+              </button>
+            </a>
           </div>
         </div>
       </div>
