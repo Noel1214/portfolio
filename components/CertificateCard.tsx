bbb2d0b8c8974dd3b2b3b8bc7e28d19344224a01
Noel Sebu <noelsebu555@gmail.com>
2025-07-02 10:51:28 +0530

    more modified Certificate card

--- components/CertificateCard.tsx
+++ components/CertificateCard.tsx
@@ -1,32 +1,41 @@
 import React from "react";
 import Image from "next/image";
+import { CiCalendar } from "react-icons/ci";
 
 interface CertificateCardProps {
   imagelink: string;
   company: string;
   desc: string;
+  linkOfCertificate: string;
+  date: string;
 }
 
 const CertificateCard: React.FC<CertificateCardProps> = (props) => {
   return (
-    <div className="interpolate-property bg-neutral-900 w-[20rem] min-h-[25rem] sm:w-[24rem] flex justify-center rounded-2xl transition-all duration-300 ease-in-out sm:max-h-none sm:max-w-none overflow-hidden hover:scale-105">
-      <div className="flex flex-col gap-2 h-full w-full items-center rounded-2 xl">
+    <div className="p-7 bg-neutral-900 w-[20rem] min-h-[25rem] sm:w-[24rem] flex justify-center rounded-2xl hover:scale-105 transition-all duration-300 ease-in-out overflow-hidden">
+      <div className="flex flex-col gap-5 h-full w-full items-center">
         <Image
-          className="h-[11rem] w-[20rem] sm:h-[13rem] sm:w-[24rem]"
+          className="h-[11rem] w-[20rem] sm:h-[13rem] sm:w-[24rem] object-contain"
           src={`/${props.imagelink}`}
           width={500}
           height={500}
           alt="Picture of the project"
         />
-        <div className="h-auto p-2 flex flex-col gap-6 scale-95">
-          <h1 className="text-2xl under line underline-offse t-7">
-            {props.company}
-          </h1>
+        <div className="h-auto flex flex-col gap-6 scale-95">
+          <div className="flex flex-col gap-2">
+            <h1 className="text-2xl">{props.company}</h1>
+            <div className="text-sm flex items-center gap-1">
+              <CiCalendar size={20} />
+              <h1 className="translate-y-[0.1rem]">{props.date}</h1>
+            </div>
+          </div>
           <h1 className="">{props.desc}</h1>
-          <div className="flex gap-3">
-            <button className="h-10 px-3 bg-neutral-700 rounded-3xl text-white  hover:cursor-pointer">
-              View Certificate
-            </button>
+          <div>
+            <a target="_blank" href={props.linkOfCertificate}>
+              <button className="h-10 px-3 bg-neutral-700 rounded-3xl text-white  hover:cursor-pointer hover:scale-105 transition-all duration-200 ease-in-out">
+                View Certificate
+              </button>
+            </a>
           </div>
         </div>
       </div>
