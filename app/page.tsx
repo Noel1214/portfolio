0ab2bd4a19f3712263cfecdcb59b3bae87438f5d
Noel Sebu <noelsebu555@gmail.com>
2025-07-03 21:20:57 +0530

    about section added

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,13 @@ import ProfileAndBio from "@/components/ProfileAndBio";
 import ProjectsSection from "@/components/ProjectsSection";
 import Certifications from "@/components/Certifications";
 import Footer from "@/components/Footer";
+import AboutMe from "@/components/AboutMe";
 
 const page = () => {
   return (
     <div className="overflow-x-hidden">
       <ProfileAndBio />
+      <AboutMe />
       <ProjectsSection />
       <Certifications />
       <Footer />
