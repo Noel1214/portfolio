9b953094ff18165854b6ab7adb12e98f555d1a7d
Noel Sebu <noelsebu555@gmail.com>
2025-07-04 12:35:04 +0530

    gave buttons gradient for dark and light theme

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -16,7 +16,7 @@ interface CertificateCardProps {
 const ProjectCard: React.FC<CertificateCardProps> = (props) => {
   return (
     <div className="px-4 pt-6 pb-5 bg-gradient-to-t from-yellow-400 to-cyan-400 dark:bg-gray-800 dark:bg-none sm:bg-neutral-900 w-[20rem] min-h-[25rem] sm:w-[24rem] flex justify-center rounded-2xl hover:scale-105 transition-all duration-300 ease-in-out overflow-hidden">
-      <div className="flex flex-col gap- h-full w-full items-center">
+      <div className="flex flex-col gap-5 h-full w-full items-center">
         <Image
           className="h-[11rem] w-[20rem] sm:h-[13rem] sm:w-[24rem] object-cover rounded-2xl"
           src={`/${props.imageLink}`}
@@ -37,12 +37,12 @@ const ProjectCard: React.FC<CertificateCardProps> = (props) => {
           <h1 className="">{props.desc}</h1>
           <div className="flex gap-4">
             <a target="_blank" href={props.demo}>
-              <button className="h-10 px-3 bg-neutral-700 rounded-3xl text-white  hover:cursor-pointer hover:scale-105 transition-all duration-200 ease-in-out">
+              <button className="h-10 px-3 bg-gradient-to-l from-[#780206] to-[#061161] dark:bg-neutral-700 dark:bg-none rounded-3xl text-white  hover:cursor-pointer hover:scale-105 transition-all duration-200 ease-in-out">
                 View Demo
               </button>
             </a>
             <a target="_blank" href={props.code}>
-              <button className="h-10 px-3 bg-neutral-700 rounded-3xl text-white  hover:cursor-pointer hover:scale-105 transition-all duration-200 ease-in-out">
+              <button className="h-10 px-3 bg-gradient-to-l from-[#780206] to-[#061161] dark:bg-neutral-700 dark:bg-none rounded-3xl text-white  hover:cursor-pointer hover:scale-105 transition-all duration-200 ease-in-out">
                 View Code
               </button>
             </a>
