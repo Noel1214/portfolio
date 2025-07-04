4850ebc90f152fd77bc5baca1c5ece1de00a0689
Noel Sebu <noelsebu555@gmail.com>
2025-07-04 12:30:13 +0530

    moved text white to parent

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,7 +7,7 @@ import { FaArrowUpLong } from "react-icons/fa6";
 
 const Footer = () => {
   return (
-    <div id="footer" className="relative mt-[8rem] p-10 bg-zinc-900">
+    <div id="footer" className="text-white relative mt-[8rem] p-10 bg-zinc-900">
       <div className="flex flex-col items-center gap-8">
         <h1 className="text-2xl text-center font-bold font-sans">
           Get In Touch
@@ -46,7 +46,7 @@ const Footer = () => {
       <div className="flex justify-center">
         <button
           onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
-          className="bg-zinc-600 text-white px-4 py-2 rounded-full shadow-lg hover:bg-zinc-800 hover:scale-125 transition-all duration-300 ease-in-out"
+          className="bg-zinc-600 px-4 py-2 rounded-full shadow-lg hover:bg-zinc-800 hover:scale-125 transition-all duration-300 ease-in-out"
         >
           <div className="flex items-center gap-2">
             <FaArrowUpLong />
