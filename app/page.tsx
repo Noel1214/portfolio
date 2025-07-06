50dbfffe022fe28afc13b6a11d3135876ef0a869
Noel Sebu <noelsebu555@gmail.com>
2025-07-06 11:26:01 +0530

    added lenis for smooth scrolling

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,28 @@
-import React from "react";
-import Navbar from "@/components/Navbar";
+"use client";
+import React, { useEffect } from "react";
 import ProfileAndBio from "@/components/ProfileAndBio";
 import ProjectsSection from "@/components/ProjectsSection";
 import Certifications from "@/components/Certifications";
 import Footer from "@/components/Footer";
 import AboutMe from "@/components/AboutMe";
+import Lenis from "lenis";
 
 const page = () => {
+  useEffect(() => {
+    // Initialize Lenis
+    const lenis = new Lenis({
+        duration: 1.5,
+    });
+
+    // Use requestAnimationFrame to continuously update the scroll
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  }, []);
+
   return (
     <div className="overflow-x-hidden">
       <ProfileAndBio />
